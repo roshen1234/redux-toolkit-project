9db28f6220eaa39f65446e92d71019195db7715c
roshen1234 <roshankoshy123@gmail.com>
2024-01-02 18:43:09 +0530

    third commit

--- src/store/slices/UserSlice.jsx
+++ src/store/slices/UserSlice.jsx
@@ -17,8 +17,16 @@ const userSlice=createSlice({
         //  console.log("hi")
         return [];
       },
-
     },
+
+// when we create another slice and if we want to use the same function like deleteUser there then there is no need to make one more there inside the reducers we can call this userSlice from that Slice and use this reducer for this we use exreaReducer code given below there 
+//     extraReducers(builders){
+//  builders.addCase(userSlice.actions.deleteUser,()=>{
+//     return [];
+//  })
+//     }
+
+
 })
 console.log(userSlice.actions)
 export default userSlice.reducer
